8a0102f75e908bb6c32c4020a00e68b8bbabe4cf
Karim <konanfoi@gmail.com>
2025-03-16 19:34:37 -0400

    Task 4 - Risk Categorization

--- cc_15.js
+++ cc_15.js
@@ -3,12 +3,28 @@
 const riskDashboard = document.getElementById("riskDashboard");//select container
 console.log("Risk Dashboard Loaded"); //print to console
 
-//Task 2 - Adding Risk Items Dynamically and Task 3:Removing Risk Items
+//Task 2 - Adding Risk Items Dynamically and Task 3:Removing Risk Items and Task 4: Risk Categorization
 
 function addRiskItem(riskName, riskLevel, department) {
     const riskCard = document.createElement("div");
     riskCard.classList.add("riskCard");
-    
+
+    switch (riskLevel) { //apply background color basec on risk level
+        case "High":
+            riskCard.style.backgroundColor = "red";
+            riskCard.style.color = "white";
+            break;
+        case "Medium":
+            riskCard.style.backgroundColor = "yellow";
+            riskCard.style.color = "black";
+            break; 
+        case "Low":
+            riskCard.style.backgroundColor = "green";
+            riskCard.style.color = "white";
+            break; 
+    }
+
+
     const nameElement = document.createElement("h3");
     nameElement.textContent = `Risk: ${riskName}`;
     
